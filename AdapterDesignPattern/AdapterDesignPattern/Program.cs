ca4b487e56210a08d014d9727a560c21ce40a2d8
turkiajq <turki.alqurashi@tuwaiq.edu.sa>
2021-06-23 00:10:06 +0300

    Adapter Design

---
+++ AdapterDesignPattern/AdapterDesignPattern/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdapterDesignPattern
+{
+	//this class olay types words in English and cannot understand other languages 
+	public class EnglishTyper
+	{
+		private ITranslator translator;
+
+		public EnglishTyper(ITranslator translator)
+		{
+			this.translator = translator;
+		}
+
+		public void Type()
+		{
+			translator.translate("Hello");
+		}
+	}
+
+	//Interface helps the English typer to understand spanish by translating the word for it
+	public interface ITranslator
+	{
+		void translate(string englishWord);
+	}
+
+	//translates the word typed by the spanish typer so the english typer can understand it
+	public class Translator : SpanishTyper, ITranslator
+	{
+		public void translate(string englishWord)
+		{
+			if (TyperInSpanish() == "Hola")
+			{
+				Console.WriteLine("Hello");
+			}
+		}
+	}
+
+	//this class can only type in Spanish 
+	public class SpanishTyper
+	{
+		public string TyperInSpanish()
+		{
+			return "Hola";
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Translator translator = new Translator();
+			EnglishTyper englishTyper = new EnglishTyper(translator);
+			englishTyper.Type();
+			Console.ReadKey();
+
+		}
+	}
+
+}
+
