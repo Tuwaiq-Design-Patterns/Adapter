890b488c68dd8178756a3be54dd57c8ba935b87b
Abdulmajeed Almaymuni <Abdulmajeed.Almaymuni@tuwaiq.edu.sa>
2021-06-22 14:51:49 +0300

    Added project files

---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+
+        public interface IFighter
+        {
+            public void attack();
+            public void defend();
+            public void escape();
+        }
+
+        public static class Wizard
+        {
+            public static string CastDestructionSpell()
+            {
+                return "Casted a very huge fireball";
+            }
+
+            public static string Shield()
+            {
+                return "Conjured a reliable and sturdy shield";
+            }
+
+            public static string Portal()
+            {
+                return "Opened a portal to another dimension";
+            }
+        }
+
+        public class WizardAdapter : IFighter
+        {
+            public void attack()
+            {
+                Console.WriteLine(Wizard.CastDestructionSpell() + " at the enemy");
+            }
+
+            public void defend()
+            {
+                Console.WriteLine(Wizard.Shield() + " to defend");
+            }
+
+            public void escape()
+            {
+                Console.WriteLine(Wizard.Portal() + " and jumped into it");
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            IFighter fighter = new WizardAdapter();
+
+            fighter.attack();
+
+            fighter.defend();
+
+            fighter.escape();
+            
+        }
+    }
+}
