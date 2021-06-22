2b165c96a7bf94c64b478af0828d47f21c5cda0d
aalhaif <Abdullah.Alhaif@Tuwaiq.edu.sa>
2021-06-22 16:59:55 +0300

    Adapter Assignment :smile:

---
+++ AdapterHW/AdapterHW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AdapterHW
+{
+    public interface ITarget
+    {
+        string GetRequest();
+    }
+
+   
+    class Target
+    {
+        public string GetOurTarget()
+        {
+            return "There You Go!";
+        }
+    }
+
+    class Adapter : ITarget
+    {
+        private readonly Target ourTarget;
+
+        public Adapter(Target adaptee)
+        {
+            this.ourTarget = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return $"Calling our target with incompatible interface ---> '{this.ourTarget.GetOurTarget()}'";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Target adaptee = new Target();
+            ITarget target = new Adapter(adaptee);
+
+            Console.WriteLine(target.GetRequest());
+        }
+    }
+}
