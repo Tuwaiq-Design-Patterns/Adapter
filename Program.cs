e7092d195f11d5d51555416ebf7ac3da3594ff28
unknown <Fatimah.Alqhtany@tuwaiq.edu.sa>
2021-06-22 18:11:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Adapter
+{
+    public interface IwebRequest
+    {
+        string GetRequest();
+    }
+
+    class Server
+    {
+        public string PostRequest()
+        {
+            return "Post request ";
+        }
+    }
+
+ 
+    class Adapter : IwebRequest
+    {
+        private readonly Server _adaptee;
+
+        public Adapter(Server adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return $"This is '{this._adaptee.PostRequest()}'";
+        }
+    }
+
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        Server webServices = new Server();
+        IwebRequest target = new Adapter(webServices);
+
+        Console.WriteLine("Server interface is incompatible with the client.");
+        Console.WriteLine("But with adapter convert to json");
+
+        Console.WriteLine(target.GetRequest());
+    }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Adapter
 this repo contains Adapter design pattern implementation using C#
+<img src"https://g.top4top.io/p_1999m7tle1.png"/>
