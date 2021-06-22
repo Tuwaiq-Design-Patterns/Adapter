224992769bb79d2999077e47c3fca4c71e79104a
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-06-22 19:56:22 +0300

    ADAPTER

---
+++ AdapterPractice/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AdapterPractice
+{
+    class Program
+    {
+
+
+
+        public interface IAdapter
+        {
+            public void getAudio(string file_name); 
+        }
+
+
+        public class Adapter : IAdapter
+        {
+            private readonly MediaPlayer _adaptee;
+
+            public Adapter(MediaPlayer adaptee)
+            {
+                this._adaptee = adaptee;
+            }
+
+
+            public void getAudio(string file_name)
+            {
+                Console.WriteLine("Playing: \t"+ file_name + this._adaptee.getFileAudio());
+            }
+        }
+
+        public class MediaPlayer
+        {
+            public string getFileAudio()
+            {
+                //adapting the file so the clinet can accept audio file.
+                return ".mp3"; 
+            }
+
+            //unused, just another example of a service.
+            public string getFilePicture() 
+            {
+                return ".png"; //*screenshots*
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MediaPlayer adaptee = new MediaPlayer();
+            IAdapter file = new Adapter(adaptee);
+
+            file.getAudio("Podcast");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
+# Bookstore
+
+### PROJECT1: 
+ASP.NET Core 5 MCV Project
+
+
+### <b> Student:</b>  Mohammed Rashed Albalawi.
+### <b> Group:</b>  Al-qiddiyah.
+### <b> Group Supervisor:</b>  Noorah Abdullah.
+
+
+<br />
+<hr />
+<br />
+
 # Adapter
 this repo contains Adapter design pattern implementation using C#
