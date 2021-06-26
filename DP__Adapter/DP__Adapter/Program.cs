ad5a8a3753e868df69b1c29fa5d927297801fd67
Samirah.Alhusayni <Samirah.alhusayni@tuwaiq.edu.sa>
2021-06-26 21:29:20 +0300

    Adapter By Samirah

---
+++ DP__Adapter/DP__Adapter/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DP__Adapter
+{
+   public interface ITarget
+    {
+        List<string> GetProducts();
+    }
+
+    public class VendorAdaptee
+    {
+        public List<string> GetListOfProducts()
+        {
+            List<string> products = new List<string>();
+            products.Add("Computer");
+            products.Add("Television");
+            products.Add("Printer");
+       
+            return products;
+        }
+    }
+
+
+
+    class VendorAdapter : ITarget
+    {
+        public List<string> GetProducts()
+        {
+            VendorAdaptee adaptee = new VendorAdaptee();
+            return adaptee.GetListOfProducts();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITarget adapter = new VendorAdapter();
+            Console.WriteLine("VendorAdaptee interface is incompatible with the client, But with VendorAdapter > client can call it's method.");
+
+            foreach (string product in adapter.GetProducts())
+            {
+                Console.WriteLine(product);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
