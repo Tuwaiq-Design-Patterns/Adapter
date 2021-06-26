5b88d388c152f8cf1ff9819d9c7339c4e40625c9
Ibrahim Alobaysi <Ibrahim.Alobaysi@tuwaiq.edu.sa>
2021-06-26 20:35:34 +0300

    Uploading The Peoject

---
+++ AdapterDemo/AdapterDemo/IFighter.cs
@@ -0,0 +1,9 @@
+namespace AdapterDemo
+{
+    public interface IFighter
+    {
+        public void Attack();
+        public void Defend();
+        public void Escape();
+    }
+}
---
+++ AdapterDemo/AdapterDemo/Wizard.cs
@@ -0,0 +1,20 @@
+namespace AdapterDemo
+{
+    public class Wizard
+    {
+        public string Spell()
+        {
+            return "Casted a lightning strike";
+        }
+
+        public string Shield()
+        {
+            return "Conjured a thick and solid shield";
+        }
+
+        public string Wings()
+        {
+            return "Opened the wings";
+        }
+    }
+}
