cb5fe4265a36e156f41dbd480ac508d506382476
Mutaz Alajlan <Mutaz.Alajlan@tuwaiq.edu.sa>
2021-06-26 15:52:09 +0300

    My first version of the adapter concept.

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -2,11 +2,42 @@
 
 namespace Adapter
 {
+    public interface UsbCharger
+    {
+        string ChargePhone();
+    }
+    class IPhone
+    {
+        public string LightningPort()
+        {
+            return "Charging IPhone with the Lightning port";
+        }
+    }
+    class UsbToLighning : UsbCharger
+    {
+        private readonly IPhone _IPhone;
+
+        public UsbToLighning(IPhone iphone)
+        {
+            this._IPhone = iphone;
+        }
+        public string ChargePhone()
+        {
+            return "The USB to Lighning adapter is: " + this._IPhone.LightningPort();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IPhone iphone = new IPhone();
+
+            UsbCharger usbCable = new UsbToLighning(iphone);
+
+            Console.WriteLine("The IPhone Lighning port is incompatible with the Usb charger. " +
+                "\n" +
+                "We need to use an adapter for our USB charger.");
+            Console.WriteLine(usbCable.ChargePhone());
         }
     }
 }
