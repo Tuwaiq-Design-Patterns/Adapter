63a845b353efcc5adbef7d7ddc8b394dfdda82bf
pivot85 <pivot8555@gmail.com>
2021-06-26 22:21:16 +0300

    files added

---
+++ AdapterDP/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AdapterDP
+{
+    public class Client
+    {
+        private ITarget target;
+
+        public Client(ITarget target)
+        {
+            this.target = target;
+        }
+
+        public void MakeRequest()
+        {
+            target.MethodA();
+        }
+    }
+
+    public interface ITarget
+    {
+        void MethodA();
+    }
+
+    public class Adapter : Adaptee, ITarget
+    {
+        public void MethodA()
+        {
+            Console.WriteLine("MethodA() is Called");
+            MethodB();
+        }
+    }
+
+    public class Adaptee
+    {
+        public void MethodB()
+        {
+            Console.WriteLine("MethodB() is called");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITarget target = new Adapter();
+            target.MethodA();
+            
+        }
+    }
+}
