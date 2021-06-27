9f2ba6ec48f0ba647d193c3829570cbad691744f
Ahmed <dev@test.com>
2021-06-27 11:30:24 +0300

    Done

---
+++ Adapter_Design_Pattern/Adapter_Design_Pattern/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Adapter_Design_Pattern
+{
+
+
+    //The Pillow python library that allow us to add text on an image but does not support arabic language
+    class Pillow
+    {
+        public string AddTextOnImage()
+        {
+            return " :( ﻊﺋﺍﺭ ﻦﻣ ﺮﺜﻛﻻﺍ ءاﺩﻻﺍ ﻰﻠﻋ هﺭﻮﻧﻭ هﺭﺎﺳ اﺮﻜﺷ ";
+        }
+    }
+
+    // The Adapter makes the Adaptee's interface compatible with the Target's
+    class Adapter 
+    {
+        private readonly Pillow _adaptee;
+
+        public Adapter(Pillow adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string AddTextOnImageThatSupportArabic()
+        {
+            char[] charArray = this._adaptee.AddTextOnImage().ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pillow adaptee = new Pillow();
+
+            Console.WriteLine("Before using Adapter:");
+
+            Console.WriteLine(adaptee.AddTextOnImage()) ;
+
+            Console.WriteLine("After using Adapter:");
+
+            Adapter arabic_language_support = new Adapter(adaptee);
+
+         
+            Console.WriteLine(arabic_language_support.AddTextOnImageThatSupportArabic());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Adapter
 this repo contains Adapter design pattern implementation using C#
+### UML
+
+   ![UML](UML.png)
