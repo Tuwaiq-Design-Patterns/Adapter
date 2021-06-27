7619cdfafe55ef07a1f0d79026372d5683fd74b1
Taghreed <Taghreed.Alqahtani@tuwaiq.edu.sa>
2021-06-27 14:06:40 +0300

    adapter design pattern

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Adapter
-this repo contains Adapter design pattern implementation using C#
+<div dir="rtl" align="right" >
+  
+  
+![Tuwaiq](https://i.ibb.co/SV2BSn5/tuwaiq.png)
+  
+  
+----
+# assignment
+
+Adapter design Pattern
+
+## UML Diagram
+
+<img src ="adapter_designPattern/UML.png" />
+
+المتدربة : تغريد القحطاني 
+
+المشرفة : نورة محسن 
+  
+</div>
---
+++ adapter_designPattern/adapter_designPattern/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace adapter_designPattern
+{
+    /// The 'Client' class
+    public class ThirdPartyBillingSystem
+    {
+        private ITarget employeeSource;
+
+        public ThirdPartyBillingSystem(ITarget employeeSource)
+        {
+            this.employeeSource = employeeSource;
+        }
+
+        public void ShowEmployeeList()
+        {
+            List<string> employee = employeeSource.GetEmployeeList();
+
+            Console.WriteLine("Employee List : ");
+            foreach (var item in employee)
+            {
+                Console.Write(item);
+            }
+
+        }
+    }
+
+    /// The 'ITarget' interface
+    public interface ITarget
+    {
+        List<string> GetEmployeeList();
+    }
+
+    /// Adaptee class
+    public class HRSystem
+    {
+        public string[][] GetEmployees()
+        {
+            string[][] employees = new string[4][];
+
+            employees[0] = new string[] { "100", "Deepak", "Team Leader" };
+            employees[1] = new string[] { "101", "Rohit", "Developer" };
+            employees[2] = new string[] { "102", "Gautam", "Developer" };
+            employees[3] = new string[] { "103", "Dev", "Tester" };
+
+            return employees;
+        }
+    }
+
+    /// Adapter class
+    public class EmployeeAdapter : HRSystem, ITarget
+    {
+        public List<string> GetEmployeeList()
+        {
+            List<string> employeeList = new List<string>();
+            string[][] employees = GetEmployees();
+            foreach (string[] employee in employees)
+            {
+                employeeList.Add(employee[0]);
+                employeeList.Add(",");
+                employeeList.Add(employee[1]);
+                employeeList.Add(",");
+                employeeList.Add(employee[2]);
+                employeeList.Add("\n");
+            }
+
+            return employeeList;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITarget Itarget = new EmployeeAdapter();
+            ThirdPartyBillingSystem client = new ThirdPartyBillingSystem(Itarget);
+            client.ShowEmployeeList();
+
+            Console.ReadKey();
+
+        }
+    }
+}
